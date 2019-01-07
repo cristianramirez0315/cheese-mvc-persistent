d97e56ee8dc65918a5c8aad3a709623e0c590012
Cristian Ramirez <cristianramirez0315@gmail.com>
2019-01-06 22:38:47 -0500

    Ready.

--- src/main/java/org/launchcode/models/data/CategoryDao.java
+++ src/main/java/org/launchcode/models/data/CategoryDao.java
@@ -1,4 +1,15 @@
 package org.launchcode.models.data;
 
-public class CategoryDao {
+import org.launchcode.models.Category;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+/**
+ * Created by LaunchCode
+ */
+@Repository
+@Transactional
+public interface CategoryDao extends CrudRepository<Category, Integer> {
 }
